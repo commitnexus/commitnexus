a63319f958af09428ef047df9e609090d7d5cce1
deepak <commitnexus@gmail.com>
2025-03-15 22:05:58 +0530

    api change

--- commitnexus-backend/api/index.js
+++
@@ -1,9 +0,0 @@
-import connectDB from "./db";
-
-export default async function handler(req, res) {
-  await connectDB(); // Ensure MongoDB is connected
-
-  res.status(200).json({
-    message: "love you nashmitha💕💕💕💕💕",
-  });
-}
